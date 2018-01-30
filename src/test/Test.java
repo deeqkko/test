99891ebff7aa71e1a58a89e79e85180919d70b0f
deeqkko <kimmo.koivusalo@gmail.com>
2018-01-30 21:24:55 +0200

    Added some code and comments to Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -16,6 +16,7 @@ public class Test {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        System.out.println("That was fun wasn't it?");
+    }   // Just some yadda-yadda code
     
 }
